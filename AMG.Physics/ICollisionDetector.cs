a6bf761ab65a32e7518c8aec2171f20f893666e1
dradamgee <dr.gee@virgin.net>
2016-04-13 17:28:40 +0100

    commit now

---
+++ AMG.Physics/ICollisionDetector.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AMG.Physics
+{
+    public interface ICollisionDetector
+    {
+        IEnumerable<Tuple<IElement, IElement>> Detect();
+    }
+}
