94b047689305793e2879de8f6be63b8018b2970e
dradamgee <dr.gee@virgin.net>
2016-04-13 16:40:45 +0100

    another commit.

---
+++ AMG.Physics.Test/TestCollisionDetector.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using Moq;
+using NUnit.Framework;
+
+namespace AMG.Physics.Test
+{
+    [TestFixture]
+    public class TestCollisionDetector
+    {
+        [Test]
+        public void TwoObjectsVeryClose_Detected()
+        {
+            Mock<IElement> e1 = new Mock<IElement>();
+            e1.SetupGet(e => e.Radius).Returns(10.0);
+            e1.SetupGet(e => e.Location).Returns(new Dimensions(100.0, 100.0));
+
+            Mock<IElement> e2 = new Mock<IElement>();
+            e2.SetupGet(e => e.Radius).Returns(10.0);
+            e2.SetupGet(e => e.Location).Returns(new Dimensions(101.0, 101.0));
+
+            var elements = new IElement[] { e1.Object, e2.Object, };
+
+            StatefullCollisionDetector detector = new StatefullCollisionDetector(elements);
+            
+//            PairCollisionDetector detector = new PairCollisionDetector(elements);
+
+            var collisionCandidates = detector.Detect().ToArray();
+            Assert.IsNotNull(collisionCandidates);
+            Assert.AreEqual(1, collisionCandidates.Length);
+            Assert.AreEqual(e1.Object, collisionCandidates[0].Item1);
+            Assert.AreEqual(e2.Object, collisionCandidates[0].Item2);
+
+
+
+        }
+    }
+}
---
+++ AMG.Physics/PairCollisionDetector.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AMG.Physics
+{
+    public class StatefullCollisionDetector : TimeDependentAction
+    {
+        private readonly IElement[] _elementsOrderedByX;
+        private readonly IElement[] _elementsOrderedByY;
+        private readonly int _count;
+
+        public StatefullCollisionDetector(IEnumerable<IElement> elements)
+        {
+            _elementsOrderedByX = elements.ToArray();
+            _elementsOrderedByY = elements.ToArray();
+            _count = _elementsOrderedByX.Length;
+        }
+
+        public override void Act()
+        {
+            var pairs = Detect().ToArray();
+            foreach (var pair in pairs) {
+                Collision.Act(pair.Item1, pair.Item2);
+            }
+        }
+
+        public IEnumerable<Tuple<IElement, IElement>> Detect() {
+            Array.Sort(_elementsOrderedByX, (e1, e2) => {
+                return e1.Location.X.CompareTo(e2.Location.X);
+            });
+            Array.Sort(_elementsOrderedByY, (e1, e2) => {
+                return e1.Location.Y.CompareTo(e2.Location.Y);
+            });
+
+            int x1 = 0;
+            while (x1 < _count)
+            {
+                int x2 = x1 + 1;
+                while (x2 < _count) 
+                {
+                    var e1 = _elementsOrderedByX[x1];
+                    var e2 = _elementsOrderedByX[x2];
+                    if  (Math.Abs(e1.Location.X - e2.Location.X) <= e1.Radius + e1.Radius) 
+                    {
+                        if (Math.Abs(e1.Location.Y - e2.Location.Y) <= e1.Radius + e1.Radius)
+                            yield return new Tuple<IElement, IElement>(e1, e2);
+                    } 
+                    else
+                    {
+                        break;
+                    }
+                    x2++;
+                }
+
+                x1++;
+            }
+        }
+    }
+
+    public class PairCollisionDetector : TimeDependentAction
+    {
+        private readonly IElement[] _elements;
+
+        public PairCollisionDetector(IEnumerable<IElement> elements)
+        {
+            _elements = elements.ToArray();
+        }
+
+        public override void Act()
+        {
+            var pairs = Detect();
+            foreach (var pair in pairs)
+            {
+                Collision.Act(pair.Item1, pair.Item2);
+            }
+        }
+
+
+        public IEnumerable<Tuple<IElement, IElement>> Detect() {
+
+            for (int i = 0; i < _elements.Length; i++) {
+                for (int j = i + 1; j < _elements.Length; j++) {
+                    var e1 = _elements[i];
+                    var e2 = _elements[j];
+
+                    if (Math.Abs(e1.Location.X - e2.Location.X) > e1.Radius + e1.Radius)
+                        continue;
+                    if (Math.Abs(e1.Location.Y - e2.Location.Y) > e1.Radius + e1.Radius)
+                        continue;
+                    
+                    yield return new Tuple<IElement, IElement>(e1, e2);
+                }
+            }
+        }
+    }
+}
