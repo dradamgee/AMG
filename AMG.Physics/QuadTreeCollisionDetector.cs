549652355831c80a2fdab6761a9fcc3bb2c37bc8
dradamgee <dr.gee@virgin.net>
2016-06-07 09:39:18 +0100

    more changes

--- AMG.Physics/QuadTreeCollisionDetector.cs
+++ AMG.Physics/QuadTreeCollisionDetector.cs
@@ -1,10 +1,20 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AMG.FySics;
 
 namespace AMG.Physics {
-    class QuadTreeCollisionDetector {
+    public class QuadTreeCollisionDetector {
+        public QuadTreeCollisionDetector(IEnumerable<IElement> elements)
+        {
+
+        }
+    }
+
+    public class QuadCell
+    {
     }
 }
