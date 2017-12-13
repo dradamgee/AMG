3cfc24b6f0c920a9b79360fbf87ab23fb394387e
Adam Gee <amg@simcorp.com>
2017-12-13 15:32:56 +0000

    commit staged changes to lorrery

---
+++ AMG.Physics.Test/UnitTest1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+
+namespace AMG.Physics.Test {
+
+    public class MyWindow
+    {
+        private bool isBusy = false;
+
+        public string Text;
+
+        private async Task<string> LongRunning()
+        {
+            await Task.Delay(10);
+            //throw new ApplicationException("Bang");
+            return "Bang";
+        }
+
+        public void Click()
+        {
+            if (isBusy)
+                return ;
+
+            var asd = Task.Factory.StartNew(() =>
+            {
+                Thread.Sleep(10);
+                throw new ApplicationException("Bang");
+            });
+
+            asd.ContinueWith(
+                t => { Text = t.Exception.InnerExceptions[0].Message;
+                       isBusy = false; }
+                , TaskContinuationOptions.NotOnRanToCompletion
+            );
+        }
+
+        public async Task ClickAsync()
+        {
+            if (isBusy)
+                return;
+
+            try
+            {
+                isBusy = false;
+                Text = await LongRunning();
+            }
+            catch (AggregateException ae)
+            {
+                isBusy = false;
+                Text = ae.InnerExceptions[0].Message;
+            }
+        }
+
+    }
+    
+    [TestFixture]
+    public class UnitTest1 {
+        [Test]
+        public async Task TestTasks() {
+            var myWindow = new MyWindow();
+            await myWindow.ClickAsync();
+            Assert.AreEqual("Bang", myWindow.Text);
+        }
+    }
+}
