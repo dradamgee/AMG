f68f7fd5f564a040922566837f608e2d875b3492
AMG <amg@simcorp.com>
2020-12-31 15:12:02 +0000

    fix broken test, there the leach was streched and it should have been slack, use a 3, 4, 5 triangle

--- AMG.FySics.Test/TestLeash.fs
+++ AMG.FySics.Test/TestLeash.fs
@@ -8,7 +8,7 @@ type TestLeash() =
     [<Test>]
     member this.WhenNotStreched_NoImpulseIsReturned() =
  
-        let leash = Leash(Vector(0.0, 0.0), 1.0, 1.0) 
+        let leash = Leash(Vector(0.0, 0.0), 5.0, 1.0) 
 
         let location = Vector(3.0, 4.0)
         let velocity = Velocity(Vector(0.0, 0.0))
